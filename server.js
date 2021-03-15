775312ae3dcf99bf9c1c92bfefa45bc6aa64ba9d
X00011999 <thomas.mayer@smail.auswaertiges-amt.de>
2021-03-15 12:15:52 +0100

    added sample code and server logic

--- server.js
+++ server.js
@@ -1,19 +1,62 @@
-// Setup empty JS object to act as endpoint for all routes
+/* Empty JS object to act as endpoint for all routes */
 projectData = {};
+let animalData = [];
 
-// Require Express to run server and routes
+/* Express to run server and routes */
+const express = require('express');
 
-// Start up an instance of app
+/* Start up an instance of app */
+const app = express();
 
+/* Dependencies */
+const bodyParser = require('body-parser')
 /* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+const cors = require('cors');
+app.use(cors());
 
-// Cors for cross origin allowance
-
-// Initialize the main project folder
+/* Initialize the main project folder*/
 app.use(express.static('website'));
 
+const port = 3000;
+/* Spin up the server*/
+const server = app.listen(port, listening);
+function listening(){
+  // console.log(server);
+  console.log(`running on localhost: ${port}`);
+};
 
-// Setup Server
+// Dummy API Endpoint
+const fakeData = {
+  animal: 'lion',
+  fact: 'lions are sweet'
+}
+
+// GET Routes
+app.get('/fakeAnimalData', getFakeData);
+
+function getFakeData(request, response) {
+  response.send(fakeData);
+}
+
+app.get('/all', getAllData);
+
+function getAllData(request, response) {
+  console.log(animalData);
+  response.send(animalData);
+}
+
+// POST Route
+app.post('/addAnimal', addAnimal);
+
+function addAnimal(request, response) {
+  newEntry = {
+    animal: request.body.animal,
+    fact: request.body.fact,
+    favFact: request.body.favFact
+  }
+
+  animalData.push(newEntry);
+  response.send(animalData);
+}
--- website/app.js
+++ website/app.js
@@ -1,5 +1,92 @@
 /* Global Variables */
+let baseUrl = "http://api.animalinfo.org/data/animals"
+let apiKey = "abc123..."
+const newAnimal = document.getElementById("animal").value;
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+document.getElementById("generate").addEventListener("click", performAction);
+
+function performAction(event) {
+    event.preventDefault();
+
+    const newAnimal = document.getElementById("animal").value;
+    const favoriteFact = document.getElementById("favorite").value;
+
+    getAnimal('/fakeAnimalData')
+    // this is new
+        .then(function(data) {
+            console.log(data);
+            // Add data to post request
+            let outData = postData('/addAnimal', {
+                animal: newAnimal,
+                fact: data.fact,
+                favFact: favoriteFact
+            });
+
+            return outData;
+        })
+        .then(function(data) { // gets the data from previous .then (outData) as input
+            console.log(data);
+            updateUI(); // get the newly persisted data over an additional get request
+        });
+};
+
+const updateUI = async() => {
+    const request = await fetch('/all');
+
+    try {
+        const allData = await request.json();
+        const previousAnimal = document.getElementById('lastAnimal').innerHTML;
+        const previousFact = document.getElementById("lastAnimalFact").innerHTML;
+        const previousFavFact = document.getElementById("lastAnimalFavFact").innerHTML;
+        
+        if(previousAnimal !== allData[allData.length-1].animal) {
+            document.getElementById('lastAnimal').innerHTML = allData[allData.length-1].animal;
+        }
+        if(previousFact !== allData[allData.length-1].fact) {
+            document.getElementById('lastAnimalFact').innerHTML = allData[allData.length-1].fact;
+        }
+        if(previousFavFact !== allData[allData.length-1].favFact) {
+            document.getElementById('lastAnimalFavFact').innerHTML = allData[allData.length-1].favFact;
+        }
+    } catch(error) {
+        console.log("Error: ", error);
+    }
+};
+
+const getAnimal = async (url) => {
+    // 1.
+    // const result = await fetch(baseUrl+animal+apiKey);
+    // 2. call fake API
+    const result = await fetch("/fakeAnimalData"); // here we send our request to the (Fake-)API
+    try {
+        const data = await result.json(); // wait until we get back data or throw an error
+        console.log(data);
+        return data;
+    } catch(error) {
+        // handle error if it exists
+        console.log("Error: ", error);
+    }
+};
+
+const postData = async (url = "", data = {}) => {
+    // console.log(data);
+    const response = await fetch(url, {
+        method: 'POST', // GET, POST, PUT, DELETE, etc.
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    });
+
+    try {
+        const newData = await response.json();
+        return newData
+    } catch(error) {
+        console.log("Error: ", error);
+    }
+};
